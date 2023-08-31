6ee4f85163451b21e42a07fa571b6df2b7d17758
Light-JH <phoebehu@outlook.com>
2023-08-31 14:37:51 -0400

    auth

--- final_project/index.js
+++ final_project/index.js
@@ -12,6 +12,10 @@ app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUni
 
 app.use("/customer/auth/*", function auth(req,res,next){
 //Write the authenication mechanism here
+    const { body } = req;
+    const { username, password } = body;
+
+    if(username === )
 });
  
 const PORT =5000;
--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -7,21 +7,43 @@ let users = [];
 
 const isValid = (username)=>{ //returns boolean
 //write code to check is the username is valid
+    if (username === users.includes(username)) {
+        return true
+    } else {
+        console.log("invalid user name")
+    }
+
 }
 
 const authenticatedUser = (username,password)=>{ //returns boolean
 //write code to check if username and password match the one we have in records.
+    if (username === regd_users.username && password === regd_users.password ) {
+        return true
+    } else {
+        console.log("invalid username or password")
+    }
 }
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
   //Write your code here
+  const {body} = req;
+  const {username, password} = body;
+  if (username === regd_users.username && password === regd_users.password) {
+      jwt.sign(user), JWT_SECRET, {expiresIn:'1h'},(err,token) => {
+          if(err) {console.log(err)}
+          res.send(token)
+      }
+  }
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
   //Write your code here
+  const { review } = req;
+  
+
   return res.status(300).json({message: "Yet to be implemented"});
 });
 
